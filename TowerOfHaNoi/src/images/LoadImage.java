9b2ce21358ab00c2918b16aa2a364733e8f0435f
thenfmt <thenf.mt@gmail.com>
2022-06-15 13:47:08 +0700

    update

---
+++ TowerOfHaNoi/src/images/LoadImage.java
@@ -0,0 +1,19 @@
+package images;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class LoadImage {
+	private BufferedImage image;
+	
+	public BufferedImage loadImage(String path) {
+		try {
+			image = ImageIO.read(getClass().getResource(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	return image;
+	}
+}
---
+++ TowerOfHaNoi/src/label/CustomLabel.java
@@ -0,0 +1,35 @@
+package label;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.IOException;
+
+import javax.swing.JLabel;
+
+public class CustomLabel extends JLabel{
+	public CustomLabel(String title, int fontSize) {
+		super(title);
+		Font font = new Font("Arial", Font.BOLD, fontSize);
+		
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, getClass().getResource("/SP3-Traveling-Typewriter.otf").openStream());
+		} catch (FontFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
+
+		GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		genv.registerFont(font);
+		font = font.deriveFont(Font.BOLD, (float)fontSize);
+		
+		
+		setFont(font);
+		setForeground(new Color(206, 202, 191));
+	}
+
+}
